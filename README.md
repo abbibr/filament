6b32d162f59d690c57048c723879d3d162a94195
abbibr <ibrohim_abbosov@mail.ru>
2025-06-13 13:37:00 +0500

    e-commerce

---
+++ README.md
@@ -0,0 +1,1 @@
+## Laravel Full E-Commerce Admin Panel
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center space-x-3">
+  <img src="{{ asset('img/logo.png') }}" alt="logo rotation" class="w-16 h-auto mx-auto my-2">
+  <h1 class="text-xl font-bold text-gray-800">Rotation</h1>
+</div>
